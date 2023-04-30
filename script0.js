73b16ed66eb4325cac1c9a29474e68f8415c38b0
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2023-05-01 00:18:36 +0200

    Create script0.js

---
+++ script0.js
@@ -0,0 +1,162 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js'
+import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js'
+import { HDRCubeTextureLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/HDRCubeTextureLoader.js'
+
+
+let material = new THREE.MeshStandardMaterial( {
+  color: 0xffffff,
+  metalness: 1.0,
+  roughness: 0.0
+} );
+
+const loader = new Rhino3dmLoader()
+loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
+
+let scene, camera, renderer, controls
+
+function init() {
+
+    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(1, 1, 1)
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.set(1, -1, 1)
+
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.physicallyCorrectLights = true;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.setPixelRatio( window.devicePixelRatio )
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    let cubeMap
+
+    cubeMap = new HDRCubeTextureLoader()
+         .setPath( 'assets/' )
+         .setDataType( THREE.UnsignedByteType )
+         .load( [ 'px.hdr', 'nx.hdr', 'py.hdr', 'ny.hdr', 'pz.hdr', 'nz.hdr' ] )
+
+
+
+    controls = new OrbitControls(camera, renderer.domElement)
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff )
+    directionalLight.intensity = 2
+    scene.add( directionalLight )
+
+    const ambientLight = new THREE.AmbientLight()
+    scene.add( ambientLight )
+
+    // Load JSON file
+      const loader = new THREE.ObjectLoader();
+    loader.load( '1.json', function ( object ) {
+
+      // set material
+      object.traverse(function (child) {
+        if (child.isMesh) {
+          child.material = material;
+        }
+      })
+
+      // clear objects from scene. do this here to avoid blink
+        scene.traverse(child => {
+            if (!child.isLight) {
+                scene.remove(child)
+            }
+        })
+
+      // add object to the scene
+      scene.add( object );
+
+     // hide spinner and enable download button
+        showSpinner(false)
+        downloadButton.disabled = false
+
+        // zoom to extents
+        zoomCameraToSelection(camera, controls, scene.children)
+
+    } )
+}
+
+/**
+ * The animation loop!
+ */
+function animate() {
+  requestAnimationFrame( animate )
+  controls.update()
+  renderer.render(scene, camera)
+}
+
+/**
+ * Helper function for window resizes (resets the camera pov and renderer size)
+  */
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  animate()
+}
+
+/**
+ * Helper function that behaves like rhino's "zoom to selection", but for three.js!
+ */
+function zoomCameraToSelection( camera, controls, selection, fitOffset = 1.2 ) {
+  
+  const box = new THREE.Box3();
+  
+  for( const object of selection ) {
+    if (object.isLight) continue
+    box.expandByObject( object );
+  }
+  
+  const size = box.getSize( new THREE.Vector3() );
+  const center = box.getCenter( new THREE.Vector3() );
+  
+  const maxSize = Math.max( size.x, size.y, size.z );
+  const fitHeightDistance = maxSize / ( 2 * Math.atan( Math.PI * camera.fov / 360 ) );
+  const fitWidthDistance = fitHeightDistance / camera.aspect;
+  const distance = fitOffset * Math.max( fitHeightDistance, fitWidthDistance );
+  
+  const direction = controls.target.clone()
+    .sub( camera.position )
+    .normalize()
+    .multiplyScalar( distance );
+  controls.maxDistance = distance * 10;
+  controls.target.copy( center );
+  
+  camera.near = distance / 100;
+  camera.far = distance * 100;
+  camera.updateProjectionMatrix();
+  camera.position.copy( controls.target ).sub(direction);
+  
+  controls.update();
+  
+}
+
+/**
+ * This function is called when the download button is clicked
+ */
+function download () {
+    // write rhino doc to "blob"
+    const bytes = doc.toByteArray()
+    const blob = new Blob([bytes], {type: "application/octect-stream"})
+
+    // use "hidden link" trick to get the browser to download the blob
+    const filename = data.definition.replace(/\.gh$/, '') + '.3dm'
+    const link = document.createElement('a')
+    link.href = window.URL.createObjectURL(blob)
+    link.download = filename
+    link.click()
+}
+
+/**
+ * Shows or hides the loading spinner
+ */
+function showSpinner(enable) {
+  if (enable)
+    document.getElementById('loader').style.display = 'block'
+  else
+    document.getElementById('loader').style.display = 'none'
+}
