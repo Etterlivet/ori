8140d2b8369c80962b6b61a918da95fff2ec23ed
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2023-05-01 14:59:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
 // initialise 'data' object that will be used by compute()
 const data = {
-  definition: '2.gh',
+  definition: 'b.gh',
   inputs: getInputs()
 }
 
